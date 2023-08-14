e57e774bb317db75365e4dde53228721fde0de71
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-14 16:11:54 -0400

    Update rtd_sphinx_project.py

--- rtd_sphinx_project.py
+++ rtd_sphinx_project.py
@@ -1,12 +1,21 @@
 
 # test name project
+"""
 
-#This is a Python docstring, we can use reStructuredText syntax here!
+
+This is a Python docstring, we can use reStructuredText syntax here!
 
 .. code-block:: python
 
+   # rtd_sphinx_project.py
+    
     # Import lumache
     #import lumache
+
+    # Call its only function
+    #get_random_ingredients(kind=["cheeses"])
+"""
+
 
    import numpy as np
 
