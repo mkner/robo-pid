e783af520c7ca474cfd536dc2586350d4f8c8714
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-14 17:03:36 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Basic Sphinx Project conf.py"
+copyright = "(c)"
+author = ""
 
 
 # -- General configuration ---------------------------------------------------
