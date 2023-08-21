0d915a911d300e3863019a3d8e70a9e434b6e2d8
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-21 17:22:49 -0400

    Update code.py

--- new_pid/code.py
+++ new_pid/code.py
@@ -1,3 +1,12 @@
+# REQUIRED: some docstring
+
+"""
+minimal RTD sphinx project needs some sort of docstring to build ?
+Python docstring - reStructuredText syntax below
+
+.. code-block:: python
+
+"""
 
 def f(x):
   return x*x
