bfef02bf9f4e0b1a96878ece5cc94274be62f78f
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-22 16:27:02 -0400

    Create stuff.py

---
+++ test/stuff.py
@@ -0,0 +1,17 @@
+
+# the usual
+
+import new_pyproject
+
+from new_pyproject import Project
+
+p = Project()
+
+p.name()
+#Out[5]: 'Project'
+
+p.desc()
+#Out[6]: 'new python project class'
+
+p.vers()
+#Out[7]: 'v0.01.07'
