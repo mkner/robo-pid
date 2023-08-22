c162928270a32c43d7d6bffeb23444e27588a823
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-22 15:11:49 -0400

    Update code.py

--- new_pyproject/code.py
+++ new_pyproject/code.py
@@ -2,8 +2,26 @@
 # code.py
 #
 
+class Project(object):
+     # new project basic form
+     def __init__(self):
+       super(Project, self).__init__()
+       self._name = "Project"
+       self._desc = "new python project class"
+       self._vers = "v0.01.02"
+      
+def name(self):
+ return self._name
+
+def desc(self):
+  return self._desc
+
+def vers(self):
+  return self._vers
+ 
 def f(x):
  return(x*x)
+ 
 
 #
 
