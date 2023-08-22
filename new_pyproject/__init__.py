75707433ca2c8afb6e0c2e21a15176dfa3120abe
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-22 15:29:29 -0400

    Update __init__.py

--- new_pyproject/__init__.py
+++ new_pyproject/__init__.py
@@ -4,7 +4,7 @@ new python project description
 """
 
 # flit uses this as one source version #
-__version__ = "0.01.02"
+__version__ = "0.01.03"
 
 # import acts to export 
 from .__init__ import __version__
