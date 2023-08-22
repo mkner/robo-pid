817b976fb04a938ba05b50cf120a7235b2cd36a8
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-22 15:55:57 -0400

    Update code.py

--- new_pyproject/code.py
+++ new_pyproject/code.py
@@ -3,24 +3,25 @@
 #
 
 class Project(object):
-     # new project basic form
+# new project basic form
+     
      def __init__(self):
        super(Project, self).__init__()
        self._name = "Project"
        self._desc = "new python project class"
-       self._vers = __version__ = "v0.01.04"
+       self._vers = __version__ = "v0.01.05"
       
-def name(self):
- return self._name
+     def name(self):
+     return self._name
 
-def desc(self):
-  return self._desc
+     def desc(self):
+       return self._desc
 
-def vers(self):
-  return self._vers
+     def vers(self):
+       return self._vers
  
-def f(x):
- return(x*x)
+     def f(x):
+      return(x*x)
  
 
 #
