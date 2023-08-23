543bef9a80c949eb60dd602db2d078468ee733e8
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-23 15:18:33 -0400

    Update __init__.py

--- new_pyproject/__init__.py
+++ new_pyproject/__init__.py
@@ -4,6 +4,7 @@ new python project description
 """
 
 # flit uses this as one source version #
+# note: pep whatever doesnt like >= 'x.y.<number>c'
 __version__ = '0.01.08c'
 
 # import acts to export 
