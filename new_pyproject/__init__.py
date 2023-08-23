fa4931cb4aa04c37730cc25bf1a9419c1a30c95a
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-23 15:25:13 -0400

    Update __init__.py

--- new_pyproject/__init__.py
+++ new_pyproject/__init__.py
@@ -4,8 +4,10 @@ new python project description
 """
 
 # flit uses this as one source version #
-# note: pep whatever doesnt like >= 'x.y.<number>c'
-__version__ = '0.01.08c'
+# note: pep whatever doesnt like > 'x.y.<number>c'
+# for example: 0.01.08d doesnt fly building from pyproject.toml
+
+__version__ = '0.01.09'
 
 # import acts to export 
 
