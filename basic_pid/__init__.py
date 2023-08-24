c877ae15e125f0a01a4f3031fd3d518f05e592cb
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-24 16:22:20 -0400

    Update __init__.py

--- basic_pid/__init__.py
+++ basic_pid/__init__.py
@@ -7,10 +7,11 @@ basic-pid - PID Controller for discretized time
 # note: pep whatever doesnt like > 'x.y.<number>c'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = '0.01.01'
+__version__ = '0.01.10a'
 
 # import acts to export 
 
 from .pid import BasicPid
+from .pid import BasicPid as Pid
 
 
