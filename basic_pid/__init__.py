96d08a7c442acfccc524fa619799e906400f781b
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-27 14:44:17 -0400

    Update __init__.py

--- basic_pid/__init__.py
+++ basic_pid/__init__.py
@@ -3,8 +3,8 @@
 basic-pid - PID Controller for discretized time
 """
 
-# flit uses this as one source version #
-# note: pep whatever doesnt like > 'x.y.<number>c'
+# flit uses __version__ as one source version 
+# note: pep versions like > 'x.y.<number>[a-c]<digit>'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
 __version__ = 'v0.01.11a'
