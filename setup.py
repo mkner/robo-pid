372b762d1399d67fb26f4381f4fe8c037acc9cae
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-05 18:43:41 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from setuptools import Command, Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+DISTNAME = "basic-pid"
+#DESCRIPTION = "pid "
+
+if __name__ == "__main__":
+    setup()
+ 
+#mk  works
+
+import basicpid
+
+#from basic_pid import 
+
