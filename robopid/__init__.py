803979adc947c80fc15b87f1a03fac05b3001ca8
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-11-05 17:11:47 -0500

    Update __init__.py

--- robopid/__init__.py
+++ robopid/__init__.py
@@ -1,17 +1,19 @@
 
 """
-robo-pid - PID Controller Mobile Robotics
+robo-pid - PID Controller for Mobile Robotics
 """
 
 # flit uses __version__ as one source version 
 # note: pep versions like > 'x.y.<number>[a-c]<digit>'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = 'v0.02.05'
+__version__ = 'v0.02.06'
 
 # import acts to export 
 
 from .pid import RoboPid
 from .pid import RoboPid as Pid
+from .pid import RoboPid as RoboPID
+
 
 
